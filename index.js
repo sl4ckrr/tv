22426df12e1c6a03109b2efa97972adc3ce110b2
Rhap5ody <domingogramos808@gmail.com>
2024-12-18 14:48:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ input.addEventListener('input', () => {
     }
   });
 });
+// background color changer
+function color(z) {
+  var a = getComputedStyle(z);
+  var b = a.backgroundColor;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor=b;
+}
