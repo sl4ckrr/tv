d4be30374f4bd6af25c05243c343d6916049f698
Rhap5ody <domingogramos808@gmail.com>
2024-12-18 14:18:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // run
   console.log('run');
+const input = document.querySelector('input');
+const games = document.querySelectorAll('#games img');
+
+input.addEventListener('input', () => {
+  const searchTerm = input.value.toLowerCase();
+  games.forEach(game => {
+    if (game.alt.toLowerCase().includes(searchTerm)) {
+      game.style.display = 'block';
+    } else {
+      game.style.display = 'none';
+    }
+  });
+});
