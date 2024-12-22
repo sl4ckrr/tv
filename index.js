bc5a021e669f5cc1d581bc1215d99a8ea747a7d2
Rhap5ody <domingogramos808@gmail.com>
2024-12-21 21:49:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-const input = document.querySelector('input');
-const games = document.querySelectorAll('#animes img');
+const express = require('express');
+const path = require('path');
 
-input.addEventListener('input', () => {
-  const searchTerm = input.value.toLowerCase();
-  games.forEach(game => {
-    if (game.alt.toLowerCase().includes(searchTerm)) {
-      game.style.display = 'block';
-    } else {
-      game.style.display = 'none';
-    }
-  });
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname));
+
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'projects.html'));
+});
+
+app.get('/bookmarklets', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bookmarklets.html'));
+});
+
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'settings.html'));
+});
+
+app.get('/support', (req, res) => {
+  res.sendFile(path.join(__dirname, 'support.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/transfer', (req, res) => {
+  res.sendFile(path.join(__dirname, 'transfer.html'));
+});
+
+app.get('/suggest', (req, res) => {
+  res.sendFile(path.join(__dirname, 'suggest.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+app.get('/ad', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ad.html'));
+});
+
+app.get('/blank', (req, res) => {
+  res.sendFile(path.join(__dirname, 'blank.html'));
+});
+app.get('/backgrounds', (req, res) => {
+  res.sendFile(path.join(__dirname, 'backgrounds.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Selenite is running on port ${port}`);
 });
